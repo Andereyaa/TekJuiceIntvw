04397bdcef1ecb72c73d7415be1873ca8480c4ee
andereyaa <andrewmamawi@gmail.com>
2024-11-12 16:08:34 +0300

    Updated Circle implementation of IModel and unit tests

--- src/Model/Circle.php
+++ src/Model/Circle.php
@@ -2,6 +2,25 @@
 
 namespace App\Model;
 
-class Circle{
-    public function __construct(){}
+class Circle implements IModel
+{
+
+    private float $radius;
+
+    public function __construct(float $radius)
+    {
+        $this->radius = $radius;
+    }
+
+    public function calculateSurface(): float
+    {
+        $area = pi() * pow($this->radius, 2);
+        return $area;
+    }
+
+    public function calculateDiameter(): float
+    {
+        $circumference = 2 * pi() * $this->radius;
+        return $circumference;
+    }
 }
