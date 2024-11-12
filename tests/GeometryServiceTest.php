18d9fa7e556a1c06f4049c258011b2b5a53f8dd4
andereyaa <andrewmamawi@gmail.com>
2024-11-12 16:36:00 +0300

    Updated geometry service test

--- tests/GeometryServiceTest.php
+++ tests/GeometryServiceTest.php
@@ -3,11 +3,30 @@
 namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
+use App\Model\Triangle;
+use App\Model\Circle;
+use App\Service\GeometryService;
 
 class GeometryServiceTest extends TestCase
 {
-    public function testSomething(): void
+    protected static $geometryService;
+    protected static $triangle;
+    protected static $circle;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+        self::$triangle = new Triangle(5, 4, 3);
+        self::$circle = new Circle(2);
+        self::$geometryService = new GeometryService();
+    }
+
+    public function testCalculatingSumOfSurfaces(): void
+    {
+        $this->assertEquals(round(self::$geometryService->getSumOfAreas(self::$triangle, self::$circle), 3, PHP_ROUND_HALF_DOWN), 18.566);
+    }
+
+    public function testCalculatingSumOfDiameters(): void
+    {
+        $this->assertEquals(round(self::$geometryService->getSumOfDiameters(self::$triangle, self::$circle), 3, PHP_ROUND_HALF_DOWN), 24.566);
     }
 }
