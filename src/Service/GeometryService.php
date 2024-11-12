991dc005245b7e541aadee024253762e222f3437
andereyaa <andrewmamawi@gmail.com>
2024-11-12 16:29:16 +0300

    Added additinal methods for geometry service

--- src/Service/GeometryService.php
+++ src/Service/GeometryService.php
@@ -4,4 +4,15 @@ namespace App\Service;
 
 use App\Model\IModel;
 
-class GeometryService {}
+class GeometryService {
+
+    public function getSumOfAreas(IModel $a, IModel $b): float {
+        $sumOfAreas = $a->calculateSurface() + $b->calculateSurface();
+        return $sumOfAreas;
+    }
+
+    public function getSumOfDiameters(IModel $a, IModel $b): float {
+        $sumOfDiameters = $a->calculateDiameter() + $b->calculateDiameter();
+        return $sumOfDiameters;
+    }
+}
